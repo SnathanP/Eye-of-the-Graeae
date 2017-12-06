bd39ea1d0c0cf234464621c8bd823fda7793122b
Thetoto <thomas.lupin@epita.fr>
2017-12-06 15:58:50 +0100

    Add about dialog + more func in GTK

--- main.c
+++ main.c
@@ -21,6 +21,52 @@ static void cb_quit (GtkWidget * p_wid, gpointer p_data)
    gtk_main_quit ();
 }
 
+static void cb_about_quit (GtkWidget * p_wid, gpointer p_data)
+{  
+  gtk_widget_destroy (p_wid);
+  (void)p_data;
+}
+
+static void cb_about (GtkWidget * p_wid, gpointer p_data)
+{
+  (void)p_wid;
+  (void)p_data;
+  GtkWidget *about_dialog;
+
+  about_dialog = gtk_about_dialog_new ();
+
+  /* Lists of authors/ documentators to be used later, they must be initialized
+   * in a null terminated array of strings.
+   */
+  const gchar *authors[] = {"Thomas LUPIN, Louis HOLLEVILLE, Arthur BUSUTIL", NULL};
+  //const gchar *documenters[] = {"", NULL};
+
+  /* We fill in the information for the about dialog */
+  gtk_about_dialog_set_program_name (GTK_ABOUT_DIALOG (about_dialog), "A propos de Eye of the Graeae");
+  gtk_about_dialog_set_copyright (GTK_ABOUT_DIALOG (about_dialog), "Copyright \xc2\xa9 2017 Bible White Corp.");
+  gtk_about_dialog_set_authors (GTK_ABOUT_DIALOG (about_dialog), authors);
+  //gtk_about_dialog_set_documenters (GTK_ABOUT_DIALOG (about_dialog), documenters);
+  gtk_about_dialog_set_website_label (GTK_ABOUT_DIALOG (about_dialog), "Eye of the Graeae website");
+  gtk_about_dialog_set_website (GTK_ABOUT_DIALOG (about_dialog), "https://eyeofthegraeae.tk");
+  GdkPixbuf *img = gdk_pixbuf_new_from_file("icon.png", NULL);
+  gtk_about_dialog_set_logo (GTK_ABOUT_DIALOG (about_dialog), img);
+  /* We do not wish to show the title, which in this case would be
+   * "AboutDialog Example". We have to reset the title of the messagedialog
+   * window after setting the program name.
+   */
+  gtk_window_set_title (GTK_WINDOW (about_dialog), "A propos");
+
+  /* To close the aboutdialog when "close" is clicked we connect the response
+   * signal to on_close
+   */
+  g_signal_connect (GTK_DIALOG (about_dialog), "response",
+                    G_CALLBACK (cb_about_quit), NULL);
+
+  /* Show the about dialog */
+  
+  gtk_widget_show (about_dialog);
+}
+
 static void cb_save (GtkWidget * p_wid, gpointer p_data)
 {  
    (void)p_wid;
@@ -112,8 +158,18 @@ g_signal_connect (
 );
 
 g_signal_connect (
+   gtk_builder_get_object (p_builder, "open"),
+   "activate", G_CALLBACK (cb_load), p_builder
+);
+
+g_signal_connect (
    gtk_builder_get_object (p_builder, "launch"),
    "clicked", G_CALLBACK (cb_main), p_builder
+);
+
+g_signal_connect (
+   gtk_builder_get_object (p_builder, "exec"),
+   "activate", G_CALLBACK (cb_main), p_builder
 );
  
 /* Signal du bouton Annuler */
@@ -123,8 +179,23 @@ g_signal_connect (
 );
 
 g_signal_connect (
+   gtk_builder_get_object (p_builder, "sav"),
+   "activate", G_CALLBACK (cb_save), p_builder
+);
+
+g_signal_connect (
+   gtk_builder_get_object (p_builder, "propos"),
+   "activate", G_CALLBACK (cb_about), NULL
+);
+
+g_signal_connect (
    gtk_builder_get_object (p_builder, "quit"),
    "activate", G_CALLBACK (cb_quit), NULL
+);
+
+g_signal_connect (
+   gtk_builder_get_object (p_builder, "win"),
+   "delete-event", G_CALLBACK (cb_quit), NULL
 );
 
 
